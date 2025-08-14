05150fd2495173878d9bb9f7e5fb495126c1e162
kakuruproject9 <kakuruproject9@gmail.com>
2025-08-14 11:37:15 +0700

    Add files via upload

---
+++ MainPage.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="カクル/KAKURU PROJECT",
+    page_icon=":tada:",
+    layout="wide"
+    )
+
+st.sidebar.success("👆SILAHKAN PILIH👆")
+
+st.title("KAKURU (Kalkulator Rumus): Cara mudah menghitung cepat soal matematika (Materi Bangun Ruang)")
+st.markdown("""
+    Orang yang terlibat :
+- Raditya Rayhan Anjaya sebagai Programmer & Pengembang
+- Jhordy Alexander sebagai Penyusun
+### Apa Itu KAKURU?
+**KAKURU** adalah sebuah mesin kalkulator yang berkerja layaknya kalkulator pada umumnya, hanya saja dirancang untuk mempermudah siswa dalam menyelesaikan masalah mengenai rumus matematika terutama dalam materi Bangun Ruang, di sini siswa dapat mengecek hasil dari jawawban yang sudah mereka jawab.
+### Gimana cara kerjanya?
+Gampang! Cuman pilih lewat sidebar di sini👈 terus tinggal satset satset deh.
+### NOTE : JANGAN SALAH GUNAKAN WEBSITE INI !!!
+""")
+
+st.subheader("Dibuat pada 2 Agustus 2025")
+st.caption("NOTE : APLIKASI INI MASIH DALAM TAHAP PENGEMBANGAN !!!")
