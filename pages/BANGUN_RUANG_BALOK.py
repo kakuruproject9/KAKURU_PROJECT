f8a79b1135518e4fd149a62749b4e614054de5f4
kakuruproject9 <kakuruproject9@gmail.com>
2025-08-14 11:47:50 +0700

    Update BANGUN_RUANG_BALOK.py

--- pages/BANGUN_RUANG_BALOK.py
+++ pages/BANGUN_RUANG_BALOK.py
@@ -1,6 +1,22 @@
 import streamlit as st
 
 st.title("BANGUN RUANG BALOK")
+st.markdown("""
+### RUMUS :
+- K = Keliling
+- L = Luas
+- V = Volume
+- p = Panjang
+- l = Lebar
+- t = Tinggi
+### RUMUS MENCARI KELILING :
+- K = 4 x (p + l + t)
+### RUMUS MENCARI LUAS :
+- L = 2 x (p x l) + (p x t) + (l x t)
+### RUMUS MENCARI VOLUME :
+- V = p x l x t
+""")
+st.subheader("👇Masukan Angka👇")
 p = st.number_input("Masukan Nilai Panjang:")
 l = st.number_input("Masukan Nilai Lebar:")
 t = st.number_input("Masukan Nilai Tinggi:")
